5f5ba383c4781ca1658c429109b53ccb96f74a77
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-14 16:47:21 +0100

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -9,15 +9,14 @@ async def upgrade(bot,update):
 	
 	**🪙 Basic** 
 	Daily  Upload  limit 20GB
-	Price Rs 49  ind /🌎 0.59$  per Month
+	
 	
 	**⚡ Standard**
 	Daily Upload limit 50GB
-	Price Rs 99  ind /🌎 1.19$  per Month
+
 	
 	**💎 Pro**
 	Daily Upload limit 100GB
-	Price Rs 179  ind /🌎 2.16$  per Month
 	
 	
 	Pay Using Upi I'd `madflixofficial@axl`
@@ -25,9 +24,9 @@ async def upgrade(bot,update):
 	After Payment Send Screenshots Of 
         Payment To Admin @calladminrobot"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/calladminrobot")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://telegra.ph/file/7f959437f9375b313ed1c.jpg"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://telegra.ph/file/30b3e45a02766803883cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("",url = "https://t.me/calladminrobot")], 
+        			[InlineKeyboardButton("",url = "https://telegra.ph/file/7f959437f9375b313ed1c.jpg"),
+        			InlineKeyboardButton("",url = "https://telegra.ph/file/30b3e45a02766803883cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
@@ -39,25 +38,23 @@ async def upgradecm(bot,message):
 	
 	**🪙 Basic** 
 	Daily  Upload  limit 20GB
-	Price Rs 49  ind /🌎 0.59$  per Month
+ 
 	
 	**⚡ Standard**
 	Daily Upload limit 50GB
-	Price Rs 99  ind /🌎 1.19$  per Month
+
 	
 	**💎 Pro**
 	Daily Upload limit 100GB
-	Price Rs 179  ind /🌎 2.16$  per Month
 	
 	
-	Pay Using Upi I'd `madflixofficial@axl`
 	
 	After Payment Send Screenshots Of 
         Payment To Admin @calladminrobot"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/calladminrobot")], 
-        			[InlineKeyboardButton("Phone Pay",url = "https://telegra.ph/file/7f959437f9375b313ed1c.jpg"),
-        			InlineKeyboardButton("Paytm Wallet/UPI",url = "https://telegra.ph/file/30b3e45a02766803883cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("",url = "https://t.me/calladminrobot")], 
+        			[InlineKeyboardButton("",url = "https://telegra.ph/file/7f959437f9375b313ed1c.jpg"),
+        			InlineKeyboardButton("",url = "https://telegra.ph/file/30b3e45a02766803883cb.jpg")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
 
 
